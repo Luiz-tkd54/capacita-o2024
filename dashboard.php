94eba3fc1b2a82d5f2f7ee8af3380724409a1120
luiz-tkd54 <lhrodrigues956@gamil.com>
2024-09-19 21:29:19 -0300

    mudança no nome da pagina

--- dashboard.php
+++ dashboard.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Index</title>
+  <title>Dashboard</title>
   <link rel="stylesheet" href="./assets/css/reset.css">
   <link rel="stylesheet" href="./assets/css/styles.css">
   <link rel="stylesheet" href="./assets/css/index.css">
